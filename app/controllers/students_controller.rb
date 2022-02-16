74935d9fed5fd7776de0d69fa793db643168e450
Michael Harris <xero5k@yahoo.com>
2022-02-16 17:46:05 -0600

    commit tree 77b3b66ead781b9f67392ab533846fb77ffbfc9a

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,12 @@
+class StudentsController < ApplicationController
+
+  def index
+    students = Student.all
+    render json: students
+  end 
+
+  def grades
+    graded = Student.order(grade: :desc)
+    render json: graded
+  end 
+end
